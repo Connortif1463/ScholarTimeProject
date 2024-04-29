e6215a92049d796d09c4b43fd2b364aea988d3ec
Connortif1463 <66648411+Connortif1463@users.noreply.github.com>
2024-04-29 00:09:31 -0400

    switching approaches

--- content.js
+++ content.js
@@ -16,49 +16,7 @@ var forbiddenWebsites = [
 
 // Check if the current URL matches any forbidden website
 if (isForbidden(window.location.href)) {
-    // Inject forbidden HTML and JavaScript directly into the current tab
-    var forbiddenHTML = `
-        <html>
-        <head>
-            <title>Forbidden Page</title>
-            <style>
-                body {
-                    margin: 0;
-                    padding: 0;
-                    height: 100vh;
-                    display: flex;
-                    justify-content: center;
-                    align-items: center;
-                    flex-direction: column;
-                    font-family: Arial, sans-serif;
-                }
-
-                h1 {
-                    margin-bottom: 20px;
-                }
-
-                button {
-                    padding: 10px 20px;
-                    font-size: 16px;
-                    cursor: pointer;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>Hello there!</h1>
-            <h3>It seems like you've tried to open a service that isn't permitted.</h3>
-            <button id="cancelBtn">Cancel</button>
-            <script>
-                document.getElementById('cancelBtn').addEventListener('click', function() {
-                    console.log("Cancel button clicked");
-                    // Navigate to Google
-                    window.location.href = "https://www.google.com/";
-                });
-            </script>
-        </body>
-        </html>
-    `;
-    document.write(forbiddenHTML);
+    chrome.runtime.sendMessage({ action: "openForbiddenPage" });
 }
 
 // Check if current URL matches any forbidden website
