7dcc6ebce6d4dcec691ff502ab2d51d3cd688d5f
Connortif1463 <66648411+Connortif1463@users.noreply.github.com>
2024-04-29 00:16:56 -0400

    background and forbidden

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 // Listen for messages from content scripts
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     console.log("Message received in background.js: ", message);
-    // Open forbidden.html in a new tab
+    // Open forbidden.html in the current tab
     if (message.action == "openForbiddenPage") {
         console.log("Opening forbidden page...");
-        chrome.tabs.create({ url: "forbidden.html" });
+        chrome.tabs.update(sender.tab.id, { url: "forbidden.html" });
     }
 });
