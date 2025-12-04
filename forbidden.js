09b6971863cb9bad426ee9450bc18c55733ecaa9
ctfis <fischet9@msu.edu>
2025-12-03 20:36:30 -0500

    changed flow and added websites to block list

--- forbidden.js
+++ forbidden.js
@@ -1,17 +1,27 @@
-// Add event listener when the document is loaded
-document.addEventListener('DOMContentLoaded', function() {
-    // Get reference to cancel button
-    var cancelBtn = document.getElementById('cancelBtn');
+// importing forbiddenWebsites
+var forbiddenWebsites = window.forbiddenWebsites || [];
 
-    // Cancel button listener
-    cancelBtn.addEventListener('click', function() {
-        console.log("Cancel button clicked");
-        if(history.length > 2){
-            history.go(-2)
+// Check if the current URL matches any forbidden website
+function isForbidden(url) {
+    for (var i = 0; i < forbiddenWebsites.length; i++) {
+        if (url.startsWith(forbiddenWebsites[i])) {
+            console.log("Current url found forbidden: " + url);
+            return true;
         }
-        else {
-            window.location.href = "https://www.google.com/";
-        }
-        
-    });
+    }
+    return false;
+}
+
+// Check if current page forbidden on page load
+if (isForbidden(window.location.href)) {
+    // Redirect the current tab to the forbidden page
+    chrome.runtime.sendMessage({ action: "openForbiddenPage" });
+}
+
+// Adds event listener when the document is loaded
+cancelBtn.addEventListener('click', function() {
+    console.log("Cancel button clicked");
+
+    // Go back 2 pages, to avoid the page that caused the issue.
+    history.go(-2);
 });
