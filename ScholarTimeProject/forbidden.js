2ddcd18c20dc18a715d8a394f7c019518302f078
Connortif1463 <ctfischetti@gmail.com>
2025-12-03 21:34:17 -0500

    reupping working code for mac, cause github stupid
    This is a version from my mac that runs successfully on chrome cause every piece of code I run on my PC doesn't seem to reliably

---
+++ ScholarTimeProject/forbidden.js
@@ -0,0 +1,27 @@
+// importing forbiddenWebsites
+var forbiddenWebsites = window.forbiddenWebsites || [];
+
+// Check if the current URL matches any forbidden website
+function isForbidden(url) {
+    for (var i = 0; i < forbiddenWebsites.length; i++) {
+        if (url.startsWith(forbiddenWebsites[i])) {
+            console.log("Current url found forbidden: " + url);
+            return true;
+        }
+    }
+    return false;
+}
+
+// Check if current page forbidden on page load
+if (isForbidden(window.location.href)) {
+    // Redirect the current tab to the forbidden page
+    chrome.runtime.sendMessage({ action: "openForbiddenPage" });
+}
+
+// Adds event listener when the document is loaded
+cancelBtn.addEventListener('click', function() {
+    console.log("Cancel button clicked");
+
+    // Go back 2 pages, to avoid the page that caused the issue.
+    history.go(-2);
+});
